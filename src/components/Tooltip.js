571dea8d751dc26433f9b71a938abc3431c4fccc
Nikk45 <nicksin36@gmail.com>
2023-08-24 18:31:59 +0530

    submit 3 4

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -3,11 +3,11 @@ import React from "react";
 const Tooltip = ({text, children})=>{
     return(
         <>
-            <h2 className="tooltiptext">{children[0]}
-                <div className="tooltip">{text[0]}</div>
+            <h2 className="tooltip">{children[0]}
+                <div className="tooltiptext">{text[0]}</div>
             </h2>
-            <p className="tooltiptext">{children[1]}
-                <div className="tooltip">{text[1]}</div>
+            <p className="tooltip">{children[1]}
+                <div className="tooltiptext">{text[1]}</div>
             </p>
         </>
     )
